448c5dc8d9794d4784856d523f19f1ff6e9c4361
Filipe Borba <48929981+filipexxborba@users.noreply.github.com>
2022-12-21 13:14:50 -0300

    `feat` Implementing the User and News type

---
+++ @types/News.ts
@@ -0,0 +1,7 @@
+export type NewsAttributes = {
+   id: number;
+   slug: string;
+   title: string;
+   description: string;
+   createdAt: Date;
+};
---
+++ @types/User.ts
@@ -0,0 +1,10 @@
+export type UserAttributes = {
+    id?: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    isActive: boolean;
+ };
+ 
+ 
